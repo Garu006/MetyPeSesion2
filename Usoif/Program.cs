c7d0d2286ce4ddb3c571533a42013b863db8c5e9
Garu006 <garojas@uamv.edu.ni>
2024-09-02 17:28:54 -0600

    Aprender el uso del if en c#

---
+++ Usoif/Program.cs
@@ -0,0 +1,31 @@
+﻿using Usoif.models;
+
+namespace Usoif
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Decir si es niño, joven o adulto
+            /*
+             *Niño: 0-12
+             *Joven: 13-25
+             *Adulto: 26 a mas
+            */
+            int edad = 0;
+            Evaluation evaluation = new Evaluation();
+            Console.WriteLine("Evaluar edad");
+            Console.WriteLine("Ingrerse tu edad: ");
+            try
+            {
+                edad = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine(evaluation.EvalAge(edad));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString);
+                
+            }
+        }
+    }
+}
---
+++ Usoif/models/Evaluation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usoif.models
+{
+    internal class Evaluation
+    {
+        public Evaluation() { }
+        public string EvalAge(int age)
+        {
+            try
+            {
+                if (age >= 0 && age <= 12) return "Eres un niño";
+                else if (age >= 13 && age <= 25) return "Eres un joven";
+                else if (age >= 26) return "Eres un adulto";
+            }
+            catch (Exception ex)
+            {
+                return $"Error{ex.ToString}";
+            }
+            return "Edad no admitida";
+        }
+    }
+}
